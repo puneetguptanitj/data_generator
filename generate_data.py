75e80ae1c9c0cdd98a529e09af03d7dbcb8edf16
Puneet Gupta <puneet.gupta2@workday.com>
2021-08-01 07:07:44 -0700

    More real timestamp of records, measure time of operation in query

--- generate_data.py
+++ generate_data.py
@@ -1,23 +1,24 @@
 import psycopg2
-from datetime import datetime
+from datetime import datetime, timedelta
 
 con = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="127.0.0.1", port="5432")
 cur = con.cursor()
 
-def insert_update_records_for_arn(arn, num):
+def insert_update_records_for_arn(arn, increments_in_hours):
     total_inserted = 0
-    batch_size = min(100 , num)
-    while(total_inserted < num):
-        insert_command = """INSERT into changes VALUES """
-        for i in range(batch_size):
-            time_of_record = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            insert_command = insert_command  + """('""" + arn + """' , '{"Resources":{"MyEC2Instance":{"Type":"AWS::EC2::Instance","Properties":{"ImageId":"ami-0ff8a91507f77f867"}}}}', """
-            insert_command = insert_command  + """'""" + str(time_of_record) + """'::timestamp, 'UPDATE'),"""
+    start_time = datetime.now()
+    end_time = datetime.now() + timedelta(days=30)
+    insert_command = """INSERT into changes VALUES """
+    while(start_time < end_time):
+        time_of_record = (start_time).strftime("%Y-%m-%d %H:%M:%S")
+        insert_command = insert_command  + """('""" + arn + """' , '{"Resources":{"MyEC2Instance":{"Type":"AWS::EC2::Instance","Properties":{"ImageId":"ami-0ff8a91507f77f867"}}}}', """
+        insert_command = insert_command  + """'""" + str(time_of_record) + """'::timestamp, 'UPDATE'),"""
         final_insert_cmd = insert_command[0:-1] + ";"
-        total_inserted += batch_size
-        print("added " + str(total_inserted) + " udpate records for arn " + arn)
-        cur.execute(final_insert_cmd)
-        con.commit()
+        total_inserted = total_inserted + 1
+        start_time = start_time+ timedelta(hours=increments_in_hours)
+    print("added " + str(total_inserted) + " udpate records for arn " + arn)
+    cur.execute(final_insert_cmd)
+    con.commit()
 
 def setup():
     # clean up 
@@ -57,11 +58,11 @@ def generate_update_records():
         arn = "arn" + str(i)
         #Todo derive below numbers from distribution
         if i < 200:
-            insert_update_records_for_arn(arn, 720)
+            insert_update_records_for_arn(arn, 1)
         elif i < 2200:
-            insert_update_records_for_arn(arn, 120)
+            insert_update_records_for_arn(arn, 6)
         elif i < 12200:
-            insert_update_records_for_arn(arn, 30)
+            insert_update_records_for_arn(arn, 24)
 
 
 setup()
--- query_data.py
+++ query_data.py
@@ -1,22 +1,26 @@
 import psycopg2
-from datetime import datetime
-from time import sleep
+import time
 
 con = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="127.0.0.1", port="5432")
 
 cur = con.cursor()
-
+batch_size = 50
 #Query to get entire snapshot of resources
-
+start  = time.time()
 cur.execute('''select changes.arn, changes.resource, changes.event_time, changes.record_type from changes 
 INNER JOIN (SELECT changes.arn, max(event_time) as max_time
 			FROM changes GROUP by arn ) as most_recent
-ON changes.arn = most_recent.arn and changes.event_time = most_recent.max_time Where changes.record_type != 'DELETE';''')
+ON changes.arn = most_recent.arn and changes.event_time = most_recent.max_time Where changes.record_type != 'DELETE' limit ''' +  str(batch_size) + ''';''')
+end = time.time()
 
+print ("Time to get all resources " + str(batch_size) + " records at a time = " + str(end-start))
 
 #Query to get timline of one resource
+start  = time.time()
+cur.execute('''select changes.arn, changes.resource, changes.event_time, changes.record_type from changes 
+INNER JOIN (SELECT changes.arn, max(event_time) as max_time
+			FROM changes GROUP by arn ) as most_recent
+ON changes.arn = most_recent.arn and changes.event_time = most_recent.max_time Where changes.record_type != 'DELETE' and changes.arn LIKE 'arn0' order by event_time DESC limit ''' +  str(batch_size) + ''';''')
+end = time.time()
 
-#cur.execute('''select changes.arn, changes.resource, changes.event_time, changes.record_type from changes 
-#INNER JOIN (SELECT changes.arn, max(event_time) as max_time
-#			FROM changes GROUP by arn ) as most_recent
-#ON changes.arn = most_recent.arn and changes.event_time = most_recent.max_time Where changes.record_type != 'DELETE' and changes.arn LIKE 'arn0';''')
+print ("Time to get resource timeline for one arn " + str(batch_size) + " records at a time = " + str(end-start))
